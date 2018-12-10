e0360199339e210578128af6417c57a27ef72492
Lee <litao@tienon.com>
2018-12-10 17:41:00 +0800

    1111111111111111

--- app/src/main/java/berry/wcg/com/berrypetsuserandroid/SplashActivity.java
+++ app/src/main/java/berry/wcg/com/berrypetsuserandroid/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Log.i("wcg01","wcg");
+        Log.i("wcg01","wgggggg");
     }
 
 }
