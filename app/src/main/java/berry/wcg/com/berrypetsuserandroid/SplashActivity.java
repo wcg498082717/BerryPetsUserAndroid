3c895fb7d2904566e4bf07af29fe21e2b61cdc22
wcg <wcg498082717@gmail.com>
2018-12-10 17:33:53 +0800

    2222222222222222

--- app/src/main/java/berry/wcg/com/berrypetsuserandroid/SplashActivity.java
+++ app/src/main/java/berry/wcg/com/berrypetsuserandroid/SplashActivity.java
@@ -2,6 +2,7 @@ package berry.wcg.com.berrypetsuserandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.i("wcg01","wcg");
     }
+
 }
